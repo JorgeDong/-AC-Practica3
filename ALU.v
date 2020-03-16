87bdeec78f44229b56a08e8172e366c42f1f1221
Jorge <jdongllaug@gmail.com>
2020-03-16 13:48:54 -0600

    Logical and Arithmetic Instructions

--- ALU.v
+++ ALU.v
@@ -22,20 +22,41 @@ module ALU
 	input [3:0] ALUOperation,
 	input [31:0] A,
 	input [31:0] B,
+	input [4:0] shamt,
 	output reg Zero,
 	output reg [31:0]ALUResult
 );
 
+localparam AND = 4'b0000;
+localparam OR  = 4'b0001;
+localparam NOR = 4'b0010;
 localparam ADD = 4'b0011;
 localparam SUB = 4'b0100;
+//Nuevos
+localparam SLL = 4'b0101;
+localparam SRL = 4'b0110;
+localparam LUI = 4'b0111;
    
    always @ (A or B or ALUOperation)
      begin
 		case (ALUOperation)
 		  ADD: // add
-			ALUResult=A + B;
+			ALUResult= A + B;
 		  SUB: // sub
-			ALUResult=A - B;
+			ALUResult= A - B;
+		  AND:
+			ALUResult= A & B;
+		  OR:
+		   ALUResult= A | B;
+		  NOR: 
+			ALUResult= ~(A|B);
+		  SLL:
+			ALUResult= A << shamt;
+		  SRL:
+		  ALUResult= A << shamt;
+		  LUI:
+			ALUResult= {B, 16'b0};
+		  //ALUResult= B << 16'b0;
 
 		default:
 			ALUResult= 0;
---
+++ sim.do
@@ -0,0 +1,4 @@
+noview wave
+vsim work.MIPS_Processor_TB
+do C:/Users/coque/Desktop/AC/AC-Practice2/wave.do
+run 200
